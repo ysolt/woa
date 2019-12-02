071ce829b12d57cf1592d277121f28116ca0ecd2
Zsolt Molnar <ysolt@cloudera.com>
2019-12-02 21:18:40 +0100

    Paginate through the whole City list in the Cloudant database

--- data_query.go
+++ data_query.go
@@ -7,8 +7,13 @@ import (
 	"time"
 )
 
-func fetchData() ([]byte, error) {
+func fetchData(bookmarkKey string) ([]byte, error) {
+
 	url := "https://mikerhodes.cloudant.com/airportdb/_design/view1/_search/geo?limit=200&q=lon:[-90%20TO%2090]%20AND%20lat:[-90%20TO%2090]"
+	if bookmarkKey != "" {
+		url += "&bookmark=" + bookmarkKey
+	}
+
 	spaceClient := http.Client{
 		Timeout: time.Second * 2, // Maximum of 2 secs
 	}
@@ -31,11 +36,21 @@ func fetchData() ([]byte, error) {
 }
 
 func getDatabaseEntries(queryresult *QueryResult) error {
-	byteValue, err := fetchData()
-	if err != nil {
-		return err
-	}
+	key := ""
+	var err error
+	var byteValue []byte
 
-	err = json.Unmarshal(byteValue, &queryresult)
+	for {
+		byteValue, err = fetchData(key)
+		if err != nil {
+			return err
+		}
+		err = json.Unmarshal(byteValue, &queryresult)
+		key = queryresult.Bookmark
+		//fmt.Println(key, len(queryresult.Rows))
+		if len(queryresult.Rows) < 200 {
+			break
+		}
+	}
 	return err
 }
