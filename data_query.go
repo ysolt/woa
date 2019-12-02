ba9c4ad4c998a0e7f575b58afaac65fc92734cdc
Zsolt Molnar <ysolt@cloudera.com>
2019-12-02 12:40:06 +0100

    Fetch one set of results from real Cloudant endpoint

--- data_query.go
+++ data_query.go
@@ -3,18 +3,42 @@ package main
 import (
 	"encoding/json"
 	"io/ioutil"
-	"os"
+	"net/http"
+	"time"
 )
 
-func getDatabaseEntries(queryresult *QueryResult) error {
-	filename := "resources/cloudant_response_example.json"
-	// Open our jsonFile
-	jsonFile, err := os.Open(filename)
+func fetchData() ([]byte, error) {
+	url := "https://mikerhodes.cloudant.com/airportdb/_design/view1/_search/geo?limit=200&q=lon:[-90%20TO%2090]%20AND%20lat:[-90%20TO%2090]"
+	spaceClient := http.Client{
+		Timeout: time.Second * 2, // Maximum of 2 secs
+	}
 
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-	// we unmarshal our byteArray which contains our
-	// jsonFile's content into 'users' which we defined above
-	json.Unmarshal(byteValue, &queryresult)
-	defer jsonFile.Close()
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	res, getErr := spaceClient.Do(req)
+	if getErr != nil {
+		return nil, err
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		return nil, err
+	}
+	//fmt.Println(string(body))
+	return body, err
+}
+
+func getDatabaseEntries(queryresult *QueryResult) error {
+	byteValue, err := fetchData()
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(byteValue, &queryresult)
 	return err
 }
